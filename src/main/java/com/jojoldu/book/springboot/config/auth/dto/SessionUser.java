458a6608c8d9f16e11f218949e354ddf253210db
moon-young-kwang <moonsunyz@naver.com>
2021-08-23 15:16:18 +0900

    구글 로그인 연동_2

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
@@ -11,7 +11,7 @@ public class SessionUser implements Serializable {
     private String email;
     private String picture;
 
-    public SessionUser(User user){
+    public SessionUser(User user) {
         this.name = user.getName();
         this.email = user.getEmail();
         this.picture = user.getPicture();
