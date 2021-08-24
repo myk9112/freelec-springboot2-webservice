9eae04d6c91cad437ba9081532117e830debc023
moon-young-kwang <moonsunyz@naver.com>
2021-08-24 15:29:42 +0900

    네이버 로그인 연동 및 테스트에 시큐리티 적용

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -25,6 +25,9 @@ public class OAuthAttributes {
     }
     // of() >> OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -38,6 +41,18 @@ public class OAuthAttributes {
                 .nameAttributeKey(userNameAttributeName)
                 .build();
 
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes){
+        Map<String, Object> response = (Map<String, Object>)attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
     //User 엔티티를 생성, 엔티티를 생성하는 시점은 처음 가입할때, 가입 시 기본권한을 Role.GUEST사용
     public User toEntity(){
